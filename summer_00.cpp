49cfd9d1b7e00c4cef546ea33f43fdcf07123611
guti15 <guti310@gmail.com>
2014-05-16 11:13:50 -0400

    "small script working with struct functions"

---
+++ summer_00.cpp
@@ -0,0 +1,40 @@
+// Robert Gutierrez 
+// using struct from youtube video 
+// link is : https://www.youtube.com/watch?v=0k23xe3c4es
+
+#include <iostream> 
+#include <string>
+using namespace std;
+
+ 
+struct student 
+{
+  string name; 
+  int grade;
+
+};
+
+int main()
+{
+  
+  student pupil1 ; 
+  student pupil2 ; 
+
+  cout << "Please enter student's name :" ; 
+  cin >> pupil1.name;
+  cout << "please enter students grade: " ; 
+  cin >> pupil1.grade; 
+
+  cout << "Please enter student's name :" ; 
+  cin >> pupil2.name; 
+  cout << "please enter students grade: " ; 
+  cin >> pupil2.grade;
+
+  cout << "Name" << "       " << "Grade" << endl;
+  
+  cout << pupil1.name << "     " << pupil1.grade << endl; 
+  cout << pupil2.name << "     " << pupil2.grade << endl;
+  
+  system ("pause");
+
+}
